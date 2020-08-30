2709aca17e13e4eecafee1c7ed31f4d5f165d4ed
Eli Gao <refraction222@gmail.com>
2020-08-30 21:46:44 +0800

    fix: move global.d.ts out from gitignored /typings

---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+declare module "@babel/plugin-syntax-optional-chaining";
+declare module "@babel/helper-plugin-test-runner" {
+  export default function(loc: string): any;
+}
+declare module "babel-plugin-tester";
